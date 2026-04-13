81c298b4f7c4b9c442625ad324c698cd74ee703c
josepheuler-maker <joseph.euler@icloud.com>
2026-04-13 14:28:39 -0600

    Initial Commit

---
+++ Gelt/GeltApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeltApp.swift
+//  Gelt
+//
+//  Created by Joseph  Euler  on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct GeltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
